453513de0f41a47e1b17b28b3b5b74fb6685f9ae
Hemang Vijaykumar Jayswal <hemangja@cybage.com>
2026-01-29 11:49:36 +0530

    added scripts

---
+++ src/scripts/install-libs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "Install GraphQL Inspector CLI tool..."
+npm install -g @graphql-inspector/cli
+
+echo "Download OpenAPI Diff CLI tool..."
+mkdir -p tools
+curl -L https://repo1.maven.org/maven2/org/openapitools/openapidiff/openapi-diff-cli/2.1.7/openapi-diff-cli-2.1.7-all.jar \
+          -o tools/openapi-diff-cli.jar
+
+echo "Fetch prod branch..."
+git fetch origin prod
+
+exit 0
