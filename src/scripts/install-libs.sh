0a7dd57820e8eea3a55263fd5963aaf394deca98
Hemang Jayswal <hemangja@cybage.com>
2026-01-29 12:49:38 +0530

    [BREAKING-APPROVED] updated

--- src/scripts/install-libs.sh
+++ src/scripts/install-libs.sh
@@ -16,19 +16,22 @@ else
   echo "GraphQL Inspector CLI already installed."
 fi
 
-echo "Download OpenAPI Diff CLI tool..."
+echo "------------------------------------------------------------------------------"
+
+echo "Install OpenAPI Diff CLI tool..."
 JAR_PATH="./tools/openapi-diff-cli.jar"
 mkdir -p tools
 if [ -f "$JAR_PATH" ]; then
-  echo "OpenAPI Diff CLI already downloaded."
+  echo "OpenAPI Diff CLI already installed."
   exit 0
 else
-  echo "Downloading OpenAPI Diff CLI..."
+  echo "OpenAPI Diff CLI not found, installing..."
   curl -L https://repo1.maven.org/maven2/org/openapitools/openapidiff/openapi-diff-cli/2.1.7/openapi-diff-cli-2.1.7-all.jar \
           -o "$JAR_PATH"
 fi
-
 ls -la tools/
+
+echo "------------------------------------------------------------------------------"
 
 echo "Fetch prod branch..."
 git fetch origin prod
