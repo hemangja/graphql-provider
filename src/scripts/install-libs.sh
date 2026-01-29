959ea286e4ab6c4c39f872c58f86adc8132eb1d1
Hemang Vijaykumar Jayswal <hemangja@cybage.com>
2026-01-29 11:59:30 +0530

    updated scripts

--- src/scripts/install-libs.sh
+++ src/scripts/install-libs.sh
@@ -7,6 +7,7 @@ echo "Download OpenAPI Diff CLI tool..."
 mkdir -p tools
 curl -L https://repo1.maven.org/maven2/org/openapitools/openapidiff/openapi-diff-cli/2.1.7/openapi-diff-cli-2.1.7-all.jar \
           -o tools/openapi-diff-cli.jar
+ls -la tools/
 
 echo "Fetch prod branch..."
 git fetch origin prod
